f8a57c3a9c45f600a13fd33583737898a81c6071
telenord <nordtelecom@yandex.ru>
2017-03-26 15:13:22 +0300

    2 урок

--- Article.php
+++ Article.php
@@ -1,14 +1,29 @@
 <?php
-require_once __DIR__ . '/Db.php';
-require_once __DIR__ . '/Model.php';
 
-class Article
-    extends Model
-{
-    protected  const TABLE = 'articles';
-    public $id;
-    public $author;
-    public $title;
-    public $lead;
+require __DIR__ . '\protected\autoload.php';
 
+if( null !==($_GET['id'])) {
+
+
+    $article = \Models\Article::findById($_GET['id']);
+
+    if(false !== $article){ ?>
+
+    <h1><?php echo $article[0]->title; ?></h1>
+    <h4> <?php echo $article[0]->author; ?></h4>
+    <p><?php echo $article[0]->lead; ?></p>
+
+    <?php }
+
+} else {
+    $news = \Models\Article::findByLimit(3);
+    ?>
+    <h2>Новости</h2>
+    <ul>
+    <?php foreach ( $news as $article){ ?>
+         <li><a href=" <?php echo "/article.php?id=" . $article->id; ?> "> <?php echo $article->title; ?> </a></li>
+    <?php } ?>
+    </ul>
+    <?php
 }
+
--- tests/index.php
+++ tests/index.php
@@ -1,9 +1,12 @@
 <?php
-require_once __DIR__ . '\..\Article.php';
+require __DIR__ . '/../protected/autoload.php';
 
-$data = Article::findById(2);
+$data = \Models\Article::findById(2);
 var_dump($data);
 
-$news1 = [ 'id'=>'5',   'lead'=>'qweqweqweasdas aWEF', 'author'=> 'Vasya', 'title'=>'bomb' ];
-
-$data = Article::add($news1);
+//$news1 = ['lead'=>"qweqweqweasdas aWEF", 'author'=> "Vasya", 'title'=>'bomb' ];
+//
+//$data = \Models\Article::insert($news1);
+//
+//
+//var_dump($data);
